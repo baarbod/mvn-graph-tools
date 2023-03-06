bb72e47e955632bc59f616b48ff0171f1da91253
Baarbod <baarbod.ashen@gmail.com>
2023-03-05 20:54:26 -0500

    added code for labeling generations

--- section_db.m
+++ section_db.m
@@ -6,14 +6,24 @@ addpath(genpath(pwd))
 
 % load full network with pials
 dat = load('networks/db_with_pial');
-G = dat.G;
-figure, plotgraph(G)
+G0 = dat.G;
+figure, plotgraph(G0)
 
 % get a section of the full network
 xrange = [0, 700];
 yrange = [0, 800];
 zrange = [-1000, 0];
-G_section = getsection(G, xrange, yrange, zrange);
-figure, plotgraph(G_section)
+G = getsection(G0, xrange, yrange, zrange);
+figure, plotgraph(G)
 
-% assign generation number to 
+% label vessel generations 
+subG = subfromedge(G, find(G.Edges.Type == 1));
+figure, plotgraph(subG)
+n1 = 1;
+n2 = 96;
+artnodes = findnodepath(G, subG, n1, n2);
+ngen = 4;
+G = labelgenerations(G, ngen, artnodes);
+figure, plotgennodes(G)
+figure, plotgenedges(G)
+
