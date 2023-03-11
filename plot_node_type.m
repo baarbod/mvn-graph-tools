093200a8790d34dba3902b0e1bd493d2079b4b2b
Baarbod <baarbod.ashen@gmail.com>
2023-03-11 18:06:27 -0500

    script to plot node types

---
+++ plot_node_type.m
@@ -0,0 +1,13 @@
+clear, clc, close all
+
+% load graph object
+dat = load('data/graphs/db_with_pial.mat');
+G = dat.G;
+
+% plot with colored edges
+figure, plotgraph(G)
+
+% plot the nodes with types colored
+figure, plotnodes(G)
+
+
